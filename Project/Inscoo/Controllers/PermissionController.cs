8f3bbf3051042b22e39ef247e61891e09f97feb1
Leo Yang in Company <453851039@qq.com>
2016-06-15 16:25:24 +0800

    permission

--- Project/Inscoo/Controllers/PermissionController.cs
+++ Project/Inscoo/Controllers/PermissionController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using Domain.Permission;
+using Microsoft.AspNet.Identity;
 using Models.Permission;
 using Services.Identity;
 using Services.Navigation;
 using Services.Permission;
+using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Web.Mvc;
 
@@ -32,11 +35,77 @@ namespace Inscoo.Controllers
             };
             return View(model);
         }
-        public ActionResult List(string rid = "")
+        public ActionResult List(string Id = "")
         {
-            var model = _permisService.GetAll(rid);
+            var model = _permisService.GetAll(Id);
             //var model = new List<PermissionViewModel>();
             return PartialView(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save()
+        {
+            try
+            {
+                string pids = Request.Form["pids"];
+                string rid = Request.Form["Id"];
+                if (!string.IsNullOrEmpty(pids))
+                {
+                    List<string> pidList = new List<string>();
+                    if (pids.Contains(","))
+                    {
+                        string[] tempStr = pids.Split(',');
+                        for (int i = 0; i < tempStr.Length; i++)
+                        {
+                            pidList.Add(tempStr[i].Trim());
+                        }
+                    }
+                    else
+                    {
+                        pidList.Add(pids);
+                    }
+                    var permissList = _permisService.GetPermissionByRole(rid);
+                    if (!permissList.Any())
+                    {
+                        foreach (var f in pidList)
+                        {
+                            var item = new PermissionItem()
+                            {
+                                func = int.Parse(f),
+                                roleId = rid
+                            };
+                            _permisService.Insert(item);
+                        }
+                    }
+                    else
+                    {
+                        foreach (var f in permissList)
+                        {
+                            if (!pidList.Contains(f.Id.ToString()))
+                            {
+                                _permisService.Delete(f);
+                            }
+                        }
+                        foreach (var f in pidList)
+                        {
+                            if (!permissList.Where(s => s.Id == int.Parse(f)).Any())
+                            {
+                                var item = new PermissionItem()
+                                {
+                                    func = int.Parse(f),
+                                    roleId = rid
+                                };
+                                _permisService.Insert(item);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("权限更改失败", e);
+            }
+            return View();
         }
     }
 }

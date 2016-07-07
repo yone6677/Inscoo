9174e3ad95df9103124c2b24e905b86971dad44d
redy <redy@REDYYONE>
2016-07-07 15:12:28 +0800

    发布到正式机

---
+++ Library/Core/Data/AppDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data
+{
+    public class AppDbContextFactory : IDbContextFactory<AppDbContext>
+    {
+        public AppDbContext Create()
+        {
+            return new AppDbContext();
+        }
+    }
+}

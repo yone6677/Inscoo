3e3c695f82f6be486c0b3acbac6e78ad2034e5fb
mazhenyu <904085444@qq.com>
2016-07-18 11:42:31 +0800

    mm

--- Project/Inscoo/Scripts/base.js
+++ Project/Inscoo/Scripts/base.js
@@ -26,24 +26,24 @@ function selectAll(e, subCheck) {
 }
 
 /*表格内？？？保险条款部分切换*/
-$(document).on('click',".toggle_color", function () {
+$(document).on('click', ".toggle_color", function () {
     $(this).parent().parent().next().siblings().children('.hideDeclare').hide();
     $(this).parent().parent().next().children().toggle();
     $(this).parent().parent().siblings().children().children().removeClass('hide_color');
-    $(this).toggleClass('hide_color');  
+    $(this).toggleClass('hide_color');
 })
 
 /*体检详情*/
-$(".health_box_body>.health_nav").on("click","li", showTab);
+$(".health_box_body>.health_nav").on("click", "li", showTab);
 function showTab(e) {
     var target = e.target || e.srcElement;
-    if (target.nodeName == "A"){
+    if (target.nodeName == "A") {
         target = target.parentNode;
     }
-    if (target.nodeName == "LI"){
+    if (target.nodeName == "LI") {
         $(this).siblings().removeClass('active');
         $(this).addClass('active');
-      
+
         var link = $(this).children().attr('href');
         $(link).addClass('show').siblings().removeClass('show');
     }
@@ -60,24 +60,92 @@ function nav(name, csskey, cssvalue) {
         }
     })
 }
-$(function(){
+$(function () {
     nav('.main-left li a', 'background', '#5E6E7E');
 })
 
 /*menu*/
 function menuNav(name, csskey, cssvalue) {
     var url = window.location.pathname + window.location.search;
+    //console.log(window.location);
+    //console.log(url);
+    
     var bgColor = "";
     $(name).each(function () {
         bgColor = $(this).attr("href");
+        var urlNow = '/' + url.split('/')[1] + '/' + url.split('/')[2] + '/';
+        if (bgColor == '/Insurance/CustomizeProduct') {
+            if (url == '/Order/Buy') {
+                url = '/Insurance/CustomizeProduct';
+            }
+            
+            if (urlNow == '/Order/EntryInfo/') {
+                url = '/Insurance/CustomizeProduct';
+            }
+            if (urlNow == '/Order/UploadFile/') {
+                url = '/Insurance/CustomizeProduct';
+            }
+            if (urlNow == '/Order/ConfirmPayment/') {
+                url = '/Insurance/CustomizeProduct';
+            }
+        }
+        if (bgColor == '/order/index') {
+            if (urlNow == '/Order/Details/') {
+                url = bgColor;
+            }
+        }
+        if (bgColor == '/order/ordertopay') {
+            if (urlNow == '/order/EntryInfo/') {
+                url = bgColor;
+            }
+            if (urlNow == '/order/UploadFile/') {
+                url = bgColor;
+            }
+        }
+        if (bgColor == '/order/CompletedOrder') {
+            if (urlNow == '/Order/Details/') {
+                url = bgColor;
+            }
+            if (urlNow == '/order/BuyMore/') {
+                url = bgColor;
+            }
+        }
+        if (bgColor == '/user/index') {
+            if (urlNow == '/user/Edit/') {
+                url = bgColor;
+            }
+        }
+        if (bgColor == '/company/listindex') {
+            if (urlNow == '/company/Edit/') {
+                url = bgColor;
+            }
+        }
+        if (bgColor == '/Nav/index') {
+            if (urlNow == '/Nav/Details/') {
+                url = bgColor;
+            }
+            if (urlNow == '/Nav/Edit/') {
+                url = bgColor;
+            }
+        }
+        if (bgColor == '/GenericAttribute/index') {
+            if (urlNow == '/GenericAttribute/Details/') {
+                url = bgColor;
+            }
+            if (urlNow == '/GenericAttribute/Edit/') {
+                url = bgColor;
+            }
+        }
         if (bgColor == url) {
             $(this).addClass('nav-background');
             $(this).parent().parent().addClass('in');
         }
     })
+
 }
 $(function () {
     menuNav('.mainNav li a');
+
 });
 
 $(".mainNav>ul>li>a").click(function () {
